2b2b7c8a761f57ca0c1d4bca8747e7547c51576a
SaiDheerajPeketi <saidheerajpeketi@gmail.com>
2023-11-14 01:11:25 +0530

    Add App Module

---
+++ app/src/main/java/com/invincible/jedishare/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.invincible.jedishare.di
+
+import android.content.Context
+import com.invincible.jedishare.data.chat.AndroidBluetoothController
+import com.invincible.jedishare.domain.chat.BluetoothController
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideBluetoothController(@ApplicationContext context: Context): BluetoothController {
+        return AndroidBluetoothController(context)
+    }
+}
