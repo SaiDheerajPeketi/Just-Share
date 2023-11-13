323253ad2197226d486fe6ebdcd55ab8ad99e5cd
Blue <amithkchoran@gmail.com>
2023-11-14 01:39:58 +0530

    Refactor Activity

--- app/src/main/java/com/invincible/jedishare/DeviceList.kt
+++ app/src/main/java/com/invincible/jedishare/DeviceList.kt
@@ -1,4 +1,4 @@
-package com.invincible.jedishare.ui.theme
+package com.invincible.jedishare
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
@@ -10,21 +10,16 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.invincible.jedishare.presentation.BluetoothViewModel
 import com.invincible.jedishare.presentation.components.ChatScreen
