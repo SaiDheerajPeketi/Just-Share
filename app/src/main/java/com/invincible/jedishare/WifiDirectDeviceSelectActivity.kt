17c4b61503dd71c6c00d97b03cf4a78a8b65573f
SaiDheerajPeketi <saidheerajpeketi@gmail.com>
2023-11-23 00:06:13 +0530

    Add Permission Handlers

--- app/src/main/java/com/invincible/jedishare/WifiDirectDeviceSelectActivity.kt
+++ app/src/main/java/com/invincible/jedishare/WifiDirectDeviceSelectActivity.kt
@@ -1,18 +1,132 @@
 package com.invincible.jedishare
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.SideEffect
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.invincible.jedishare.presentation.PermissionViewModel
+import com.invincible.jedishare.presentation.components.InternetPermissionTextProvider
+import com.invincible.jedishare.presentation.components.LocationPermissionTextProvider
+import com.invincible.jedishare.presentation.components.PermissionDialog
+import com.invincible.jedishare.presentation.components.WifiPermissionTextProvider
 import com.invincible.jedishare.presentation.ui.theme.JediShareTheme
 
 class WifiDirectDeviceSelectActivity : ComponentActivity() {
+    private val permissionsToRequest = if(Build.VERSION.SDK_INT >= 33) {
+        arrayOf(
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.CHANGE_NETWORK_STATE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.FOREGROUND_SERVICE
+        )
+    }
+    else if(Build.VERSION.SDK_INT >= 28){
+        arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.CHANGE_NETWORK_STATE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.FOREGROUND_SERVICE
+        )
+    }
+    else{
+        arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.CHANGE_WIFI_STATE,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.CHANGE_NETWORK_STATE,
+            Manifest.permission.INTERNET
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JediShareTheme {
+                val permissionViewModel = viewModel<PermissionViewModel>()
+                val dialogQueue = permissionViewModel.visiblePermissionDialogQueue
 
+                val multiplePermissionResultLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestMultiplePermissions(),
+                    onResult = { perms ->
+                        permissionsToRequest.forEach { permission ->
+                            permissionViewModel.onPermissionResult(
+                                permission = permission,
+                                isGranted = perms[permission] == true
+                            )
+                        }
+                    }
+                )
+
+                SideEffect {
+                    multiplePermissionResultLauncher.launch(permissionsToRequest)
+                }
+
+                dialogQueue
+                    .reversed()
+                    .forEach { permission ->
+                        PermissionDialog(
+                            permissionTextProvider = when (permission) {
+                                Manifest.permission.INTERNET -> {
+                                    InternetPermissionTextProvider()
+                                }
+                                Manifest.permission.ACCESS_NETWORK_STATE -> {
+                                    InternetPermissionTextProvider()
+                                }
+                                Manifest.permission.CHANGE_NETWORK_STATE -> {
+                                    InternetPermissionTextProvider()
+                                }
+                                Manifest.permission.ACCESS_COARSE_LOCATION -> {
+                                    LocationPermissionTextProvider()
+                                }
+                                Manifest.permission.ACCESS_FINE_LOCATION -> {
+                                    LocationPermissionTextProvider()
+                                }
+                                Manifest.permission.ACCESS_WIFI_STATE -> {
+                                    WifiPermissionTextProvider()
+                                }
+                                Manifest.permission.CHANGE_WIFI_STATE -> {
+                                    WifiPermissionTextProvider()
+                                }
+                                else -> return@forEach
+                            },
+                            isPermanentlyDeclined = !shouldShowRequestPermissionRationale(
+                                permission
+                            ),
+                            onDismiss = permissionViewModel::dismissDialog,
+                            onOkClick = {
+                                permissionViewModel.dismissDialog()
+                                multiplePermissionResultLauncher.launch(
+                                    arrayOf(permission)
+                                )
+                            },
+                            onGoToAppSettingsClick = ::openAppSettings
+                        )
+                    }
             }
         }
     }
 }
 
+fun Activity.openAppSettings() {
+    Intent(
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", packageName, null)
+    ).also(::startActivity)
+}
---
+++ app/src/main/java/com/invincible/jedishare/presentation/components/PermissionDialog.kt
@@ -0,0 +1,105 @@
+package com.invincible.jedishare.presentation.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PermissionDialog(
+    permissionTextProvider: PermissionTextProvider,
+    isPermanentlyDeclined: Boolean,
+    onDismiss: () -> Unit,
+    onOkClick: () -> Unit,
+    onGoToAppSettingsClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        buttons = {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Divider()
+                Text(
+                    text = if(isPermanentlyDeclined) {
+                        "Grant permission"
+                    } else {
+                        "OK"
+                    },
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            if (isPermanentlyDeclined) {
+                                onGoToAppSettingsClick()
+                            } else {
+                                onOkClick()
+                            }
+                        }
+                        .padding(16.dp)
+                )
+            }
+        },
+        title = {
+            Text(text = "Permission required")
+        },
+        text = {
+            Text(
+                text = permissionTextProvider.getDescription(
+                    isPermanentlyDeclined = isPermanentlyDeclined
+                )
+            )
+        },
+        modifier = modifier
+    )
+}
+
+interface PermissionTextProvider {
+    fun getDescription(isPermanentlyDeclined: Boolean): String
+}
+
+class InternetPermissionTextProvider: PermissionTextProvider {
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if(isPermanentlyDeclined) {
+            "It seems you permanently declined internet permission. " +
+                    "You can go to the app settings to grant it."
+        } else {
+            "This app needs access to your internet so that it can use " +
+                    "standard java sockets."
+        }
+    }
+}
+
+class LocationPermissionTextProvider: PermissionTextProvider {
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if(isPermanentlyDeclined) {
+            "It seems you permanently declined location permission. " +
+                    "You can go to the app settings to grant it."
+        } else {
+            "This app needs access to your location so that you can " +
+                    "find your friends ."
+        }
+    }
+}
+
+class WifiPermissionTextProvider: PermissionTextProvider {
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if(isPermanentlyDeclined) {
+            "It seems you permanently declined wifi permission. " +
+                    "You can go to the app settings to grant it."
+        } else {
+            "This app needs wifi permission so that you can transfer " +
+                    "files to your friends using Wifi Direct."
+        }
+    }
+}
