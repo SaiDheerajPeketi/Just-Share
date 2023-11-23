f4fa2580cee7ecad3d4a1061f7efc816a797395e
Pranav Sanand Puligandla <pranav.sanand.p@gmail.com>
2023-11-23 21:53:01 +0530

    Change Navbar

--- app/src/main/java/com/invincible/jedishare/HistoryActivity.kt
+++ app/src/main/java/com/invincible/jedishare/HistoryActivity.kt
@@ -3,6 +3,8 @@ package com.invincible.jedishare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -12,32 +14,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.invincible.jedishare.ui.theme.JediShareTheme
 
+
 class HistoryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JediShareTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background),
+                    contentAlignment = androidx.compose.ui.Alignment.BottomCenter,
                 ) {
-                    Greeting2("Android")
+                    NavBar()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting2(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview2() {
-    JediShareTheme {
-        Greeting2("Android")
     }
 }
--- app/src/main/java/com/invincible/jedishare/MainActivity.kt
+++ app/src/main/java/com/invincible/jedishare/MainActivity.kt
@@ -2,23 +2,14 @@ package com.invincible.jedishare
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.Window
-import android.view.WindowManager
-import android.widget.Switch
 import androidx.activity.ComponentActivity
 import com.invincible.jedishare.ui.*
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectTransformGestures
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -28,34 +19,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Switch
-import androidx.compose.material.SwitchDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -68,15 +46,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -88,7 +62,6 @@ import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.startActivity
 import com.invincible.jedishare.ui.theme.JediShareTheme
 import com.invincible.jedishare.ui.theme.MyRed
-import com.invincible.jedishare.ui.theme.MyRedSecondary
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -437,7 +410,11 @@ fun NavBar() {
             },
             label = { Text(text = "Settings") },
             selected = false,
-            onClick = { /*TODO*/ },
+            onClick = {
+                context?.finish()
+                val intent = Intent(context, SettingsActivity ::class.java)
+                context?.startActivity(intent)
+            },
             alwaysShowLabel = true,
             selectedContentColor = Color.Black,
             unselectedContentColor = Color.Gray,
@@ -464,19 +441,22 @@ fun NavBar() {
         )
         BottomNavigationItem(
             icon = {
+                val icon: Painter = painterResource(id = R.drawable.history_icon)
                 Icon(
-                    imageVector = Icons.Default.Info,
+                    painter = icon ,
                     contentDescription = "History",
                     tint = iconColor,
                     modifier = Modifier.size(24.dp)
                 )
+
+                
             },
             label = { Text(text = "History") },
             selected = false,
             onClick = {
-//                context?.finish()
-//                val intent = Intent(context,)
-
+                context?.finish()
+                val intent = Intent(context, HistoryActivity ::class.java)
+                context?.startActivity(intent)
             },
             alwaysShowLabel = true,
             selectedContentColor = Color.Black,
